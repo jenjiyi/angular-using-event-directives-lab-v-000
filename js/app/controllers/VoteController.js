6709fd32c530cd441e86b011b4945a2404a1f3bc
JENNIFER YAMADA <jenny.yamada@gmail.com>
2016-09-28 11:15:44 -0700

    done

--- js/app/controllers/VoteController.js
+++ js/app/controllers/VoteController.js
@@ -1,12 +1,31 @@
 function VoteController() {
     this.votes = 0;
+    var voteIncrease = false;
+    var voteDecrease = false;
+
 
     this.incrementVotes = function () {
-
+      if (voteIncrease == false){
+      this.votes++;
+      voteIncrease = true;
+      voteDecrease = false;
+    } else if (voteIncrease == true){
+        this.votes--;
+        voteIncrease = false;
+        voteDecrease = false;
+    }
     };
 
     this.decrementVotes = function () {
-
+      if ( voteDecrease == false) {
+        this.votes--;
+        voteIncrease = false;
+        voteDecrease = true;
+      } else if ( voteDecrease == true) {
+        this.votes++;
+        voteIncrease = false;
+        voteDecrease = false;
+      }
     };
 }
 
